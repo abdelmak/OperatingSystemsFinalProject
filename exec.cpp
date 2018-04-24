6e8b9b779d62cc718928d18d7887506534903c30
Faris Abdel-Maksoud <31115497+abdelmak@users.noreply.github.com>
2018-04-24 10:10:55 -0400

    Add files via upload

---
+++ exec.cpp
@@ -0,0 +1,45 @@
+
+
+
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+using namespace std;
+
+string exec(const char*);
+
+
+int main()
+{
+    string[] input;
+    bool cont = true;
+    while(cont){
+        cin >> input;
+        if(input == "exit")
+            cont = false;
+        cout << exec(input.c_str()) << endl;
+    }
+}
+
+
+
+string exec(const char* cmd) {
+    array<char, 128> buffer;
+    string result;
+    shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+
+    if (!pipe) 
+    throw 
+        runtime_error("popen() failed!");
+
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+            result += buffer.data();
+    }
+
+    return result;
+}
